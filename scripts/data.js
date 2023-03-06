e84a42f31cea1424441b9554c49223840924171c
YIOXIO <gekkoivan@gmail.com>
2023-03-06 13:41:31 +0300

    updated

--- scripts/data.js
+++ scripts/data.js
@@ -1,27 +1,27 @@
 const initialCards = [
     {
-        name: 'London, UK',
-        link: 'https://images.unsplash.com/photo-1677708502185-cf21038b1ac6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'
+        name: 'Otherwise by Morcheeba',
+        link: 'https://avatars.yandex.net/get-music-content/33216/936f6bc8.a.154906-1/m1000x1000?webp=false'
     },
     {
-        name: 'Stavanger, Norway',
-        link: 'https://images.unsplash.com/photo-1677775204944-47e13173a2c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'
+        name: 'Song of crickets by Parov Stelar',
+        link: 'https://is2-ssl.mzstatic.com/image/thumb/Music69/v4/72/6f/61/726f6198-a693-8049-6ca8-ff094c2be671/cover.jpg/1200x1200bf-60.jpg'
     },
     {
         name: 'Albufeira, Portugal',
-        link: 'https://images.unsplash.com/photo-1677824744961-5bac940caaf7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=626&q=80'
+        link: 'https://i.ytimg.com/vi/xm2lpKwhHug/maxresdefault.jpg'
     },
     {
-        name: 'Munich, Germany',
-        link: 'https://images.unsplash.com/photo-1672522597762-63bb63b4f1d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        name: 'Web of Deception by Thievery Corporation',
+        link: 'https://is5-ssl.mzstatic.com/image/thumb/Music114/v4/aa/9f/f2/aa9ff27c-0525-8706-7b22-427b519b9db0/795103017726_Cover.jpg/1200x1200bf-60.jpg'
     },
     {
-        name: 'Kandersteg, Schweiz',
-        link: 'https://images.unsplash.com/photo-1677758363634-51c1f1f97f05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        name: 'Do Not Go Quietly Unto Your Grave by Morphine',
+        link: 'https://i.pinimg.com/736x/80/b6/95/80b695a189fdc75cce528962bb7f0587.jpg'
     },
     {
-        name: 'Kyoto, Japan',
-        link: 'https://images.unsplash.com/photo-1624253346805-df69ad2b3d7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        name: 'Stoned and Alone by Findlay',
+        link: 'https://rocknfolk-cdn.respawn.fr/wp-content/uploads/2017/02/fpleasure.jpg'
     }
 ];
 
--- scripts/script.js
+++ scripts/script.js
@@ -19,6 +19,8 @@ const placeInput = cardForm.querySelector('.popup__field_place');
 
 
 
+
+
 // Открыть профиль
 function openProfile() {
     popup.classList.add('popup_opened');
